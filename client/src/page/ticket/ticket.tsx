a6170a9419378728f4bc170d192108b1f5aecc76
Ваше имя <@.com>
2025-03-09 17:11:38 +0300

    fix

--- client/src/page/ticket/ticket.tsx
+++ client/src/page/ticket/ticket.tsx
@@ -4,8 +4,12 @@ import './ticket.scss'
 const Ticket = () => {
     return (
         <>
+            <div className="tocket-block">
+                <span>Сообщение</span>
+                <textarea class="inputField--mgSyJ" name="message"></textarea>
+            </div>
             <span className="application">Оставье заявку</span>
-            <Frames/>
+            <Frames />
         </>
     )
 }
